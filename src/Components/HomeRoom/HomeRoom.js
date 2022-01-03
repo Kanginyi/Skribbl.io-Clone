a676602218dd6bb2feaae9ae8744e3da0faf02f2
Samuel <samlee1097@yahoo.com>
2022-01-02 20:07:03 -0500

    Created a home component

---
+++ src/Components/HomeRoom/HomeRoom.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {FaPaintBrush} from "react-icons/fa";
+
+function HomeRoom() {
+    return (
+        <div className="App">
+        <header id="homepage-header">
+            <h1 id="rainbow-title">
+                <span>S</span>
+                <span>a</span>
+                <span>m</span>
+                <span>r</span>
+                <span>i</span>
+                <span>c</span>
+                <span>.</span>
+                <span>io</span>
+                &nbsp;
+                <FaPaintBrush id="paintbrush"/>
+            </h1>
+        </header>
+        
+    </div>
+    );
+}
+
+export default HomeRoom;
