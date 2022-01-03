31a27c6ce5409a63a578453f465927e6f9fb3076
Eric Yi <erickanginyi@gmail.com>
2022-01-02 19:21:44 -0500

    Separate letters into individual spans

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import {FaPaintBrush} from "react-icons/fa";
-import "bootstrap/dist/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <header>
-            <h1>Samric.io <FaPaintBrush/></h1>
+        <header id="homepage-header">
+            <h1 id="rainbow-title">
+                <span>S</span>
+                <span>a</span>
+                <span>m</span>
+                <span>r</span>
+                <span>i</span>
+                <span>c</span>
+                <span>.</span>
+                <span>io</span>
+                &nbsp;
+                <FaPaintBrush id="paintbrush"/>
+            </h1>
         </header>
         
 
