809ac95e8871d48aa47160805cad619923c2e79f
Samuel <samlee1097@yahoo.com>
2022-01-02 20:56:54 -0500

    create draw time in seconds

--- src/Components/PrivateRoom/Settings.js
+++ src/Components/PrivateRoom/Settings.js
@@ -8,8 +8,30 @@ function Settings() {
                <h3>Lobby</h3>
                <div id="rounds-container">
                <p><strong>Rounds</strong></p>
-                <select id = "rounds"> 
-                        <option>2</option>
+                    <select id = "rounds"> 
+                        <option className='round-entry'>2</option>
+                        <option className='round-entry'>3</option>
+                        <option className='round-entry'>4</option>
+                        <option className='round-entry'>5</option>
+                        <option className='round-entry'>6</option>
+                        <option className='round-entry'>7</option>
+                        <option className='round-entry'>8</option>
+                        <option className='round-entry'>9</option>
+                        <option className='round-entry'>10</option>
+                    </select>
+               </div>
+
+               <div id="draw-time-container">
+               <p><strong>Draw time in seconds</strong></p>
+                    <select id = "rounds"> 
+                        <option className='round-entry'>30</option>
+                        <option className='round-entry'>40</option>
+                        <option className='round-entry'>50</option>
+                        <option className='round-entry'>60</option>
+                        <option className='round-entry'>70</option>
+                        <option className='round-entry'>80</option>
+                        <option className='round-entry'>90</option>
+                        <option className='round-entry'>100</option>
                     </select>
                </div>
            </div>
