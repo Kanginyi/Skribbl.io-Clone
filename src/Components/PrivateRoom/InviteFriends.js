df43bc3f9ef5816744472fd90d57bfde1c9fa7f3
Samuel <samlee1097@yahoo.com>
2022-01-03 18:43:20 -0500

    create hover effect on link

--- src/Components/PrivateRoom/InviteFriends.js
+++ src/Components/PrivateRoom/InviteFriends.js
@@ -5,7 +5,7 @@ function InviteFriends() {
     return (
        <div id="friend-invite-container">
            <h2 id='private-room-invite'>Invite your friends!</h2>
-           <p id="invite-link">Hover over me to see the invite link!</p>
+           {showLink ? <p id="secret-invite-link" onMouseOut={()=>setShowLink(false)}>Its me the secret link</p> : <p id="invite-link" onMouseOver={()=>setShowLink(true)}>Hover over me to see the invite link!</p>}
        </div>
     );
 }
