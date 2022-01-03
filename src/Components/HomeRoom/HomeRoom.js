177cd75724b9db1417aa1557a18e65f440f4d8e7
Eric Yi <erickanginyi@gmail.com>
2022-01-03 01:21:47 -0500

    Remove duplicate header

--- src/Components/HomeRoom/HomeRoom.js
+++ src/Components/HomeRoom/HomeRoom.js
@@ -7,22 +7,7 @@ import "../../Stylings/HomeRoom.css";
 function HomeRoom() {
     return (
         <div className="App">
-        <header id="homepage-header">
-            <h1 id="rainbow-title">
-                <span>S</span>
-                <span>a</span>
-                <span>m</span>
-                <span>r</span>
-                <span>i</span>
-                <span>c</span>
-                <span>.</span>
-                <span>io</span>
-                &nbsp;
-                <FaPaintBrush id="paintbrush"/>
-            </h1>
-        </header>
-    </div>
-=======
+
             <header id="homepage-header">
                 <h1 id="rainbow-title">
                     <span>S</span>
