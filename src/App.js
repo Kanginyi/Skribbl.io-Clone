2e0d8b6c2b9fa65c55a137be8e844e238d68bd06
Samuel <samlee1097@yahoo.com>
2022-01-02 20:06:59 -0500

    Created a home component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-import {FaPaintBrush} from "react-icons/fa";
+
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import GameRoom from "./Components/GameRoom/GameRoom";
+import HomeRoom from "./Components/HomeRoom/HomeRoom"
 
 function App() {
   return (
       <Router>
         <Routes>
-          <Route path="/" element={
-            <div className="App">
-                <header id="homepage-header">
-                    <h1 id="rainbow-title">
-                        <span>S</span>
-                        <span>a</span>
-                        <span>m</span>
-                        <span>r</span>
-                        <span>i</span>
-                        <span>c</span>
-                        <span>.</span>
-                        <span>io</span>
-                        &nbsp;
-                        <FaPaintBrush id="paintbrush"/>
-                    </h1>
-                </header>
-                
-            </div>
-           }/>
-
+            <Route path="/" element={<HomeRoom/>}/>
             <Route path="gameroom" element={<GameRoom/>}/>
         </Routes>
     </Router>
