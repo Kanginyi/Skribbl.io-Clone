4f3211f7c2d267fba6824079cae508f10e61d920
Eric Yi <erickanginyi@gmail.com>
2022-01-02 19:55:21 -0500

    Add react-router-dom elements to handle routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import {FaPaintBrush} from "react-icons/fa";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import GameRoom from "./Components/GameRoom/GameRoom";
 
 function App() {
   return (
-    <div className="App">
-        <header id="homepage-header">
-            <h1 id="rainbow-title">
-                <span>S</span>
-                <span>a</span>
-                <span>m</span>
-                <span>r</span>
-                <span>i</span>
-                <span>c</span>
-                <span>.</span>
-                <span>io</span>
-                &nbsp;
-                <FaPaintBrush id="paintbrush"/>
-            </h1>
-        </header>
-        
+      <Router>
+        <div className="App">
+            <header id="homepage-header">
+                <h1 id="rainbow-title">
+                    <span>S</span>
+                    <span>a</span>
+                    <span>m</span>
+                    <span>r</span>
+                    <span>i</span>
+                    <span>c</span>
+                    <span>.</span>
+                    <span>io</span>
+                    &nbsp;
+                    <FaPaintBrush id="paintbrush"/>
+                </h1>
+            </header>
+            
+        </div>
 
-    </div>
+        <Routes>
+            <Route path="gameroom" element={<GameRoom/>}/>
+        </Routes>
+    </Router>
   );
 }
 
