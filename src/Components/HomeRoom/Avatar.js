c580a01419390c39c569ce7f04ff616b42322c7b
Eric Yi <erickanginyi@gmail.com>
2022-01-04 20:24:09 -0500

    Create file and populate with placeholder data

---
+++ src/Components/HomeRoom/Avatar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as style from '@dicebear/avatars-avataaars-sprites';
+
+function Avatar() {
+    return (
+        <h1>sd</h1>
+    );
+}
+
+export default Avatar;
